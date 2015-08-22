842b04f6b02f387f396990f736c45d9388b2ef62
Li Xuanji <xuanji@gmail.com>
2015-08-22 07:13:52 -0400

    full dump-to-mongo flow

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ from flask import Flask, redirect, make_response, request
 import sys
 import requests
 from pymongo import MongoClient
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 with open('POCKET_API_KEY') as f:
     POCKET_API_KEY = f.readlines()[0]
+
+with open('MONGOLAB_PASSWORD') as f:
+    MONGOLAB_PASSWORD = f.readlines()[0]
+
+connection = MongoClient("ds033153.mongolab.com", 33153)
+db = connection["pocket-dla"]
+db.authenticate("xuanji", MONGOLAB_PASSWORD)
+
+mongo_ru = db.resolved_urls
+mongo_texts = db.texts
+
 JSON_HEADERS = {'Content-type': 'application/json; charset=UTF-8', 'X-Accept': 'application/json'}
 
 @app.route('/')
@@ -43,22 +55,77 @@ def receive_oauth():
     access_token = r.json()['access_token']
     username = r.json()['username']
 
+    if (mongo_ru.count({
+        'username': username,
+    })):
+        return 'already added'
+
     r = requests.post('https://getpocket.com/v3/get', json={
         'consumer_key': POCKET_API_KEY,
         'access_token': access_token,
         'state': 'all',
-        'count': 5,
         'detailType': 'simple'
     }, headers=JSON_HEADERS)
 
     resolved_urls = [item['resolved_url'] for item in r.json()['list'].values()]
-    print(resolved_urls)
+
+    mongo_ru.insert({
+        'username': username,
+        'resolved_urls': resolved_urls,
+    })
+
+    mongo_texts.insert([{
+        'resolved_url': url
+    } for url in resolved_urls])
+
+    return 'added'
+
+def resolve(url):
+
+    print('resolve', url)
+
+    text_url = ''
+    text_url += 'http://text.readitlater.com/v3beta/text'
+    text_url += '?images=0&output=json&msg=1'
+    text_url += '&url=' + url
+
+    r = requests.get(text_url)
+
+    html = r.json()['article']
+
+    if (html == False):
+        return ''
+
+    soup = BeautifulSoup(html, 'html.parser')
+    return soup.get_text()
+
+@app.route('/work-on-texts')
+def work_on_text():
+
+    naked_texts = mongo_texts.find({
+        'article': None,
+    }, limit=5)
+
+    for naked_text in naked_texts:
+        url = naked_text['resolved_url']
+        _id = naked_text['_id']
+
+        print(_id)
+
+        mongo_texts.update({
+            'resolved_url': url,
+        }, {
+            '$set': {
+                'article': resolve(url)
+            }
+        })
+
+    return str(mongo_texts.count({
+        'article': None,
+    }))
 
 
-
-    return 'oauth redirect'
-
-port = int(sys.argv[1]) or 5000
+port = int(sys.argv[1])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port, debug=True)
